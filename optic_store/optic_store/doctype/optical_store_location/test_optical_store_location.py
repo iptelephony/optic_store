d507c6ff27061dd0b74e5157a1ba5670cf9504c9
Leo <leo@jirehgroup.asia>
2022-02-28 10:47:09 +0800

    Update visioncare plan subscription

---
+++ optic_store/optic_store/doctype/optical_store_location/test_optical_store_location.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, 9T9IT and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestOpticalStoreLocation(unittest.TestCase):
+	pass
---
+++ optic_store/optic_store/doctype/patient_eye_condition/patient_eye_condition.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, 9T9IT and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Patient Eye Condition', {
+	// refresh: function(frm) {
+
+	// }
+});
