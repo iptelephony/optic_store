5b6d1762d7e96bd657aae220183d9e909694c28e
Zwe Htet Aung <zwe@erpdev.djrogvrrsbiubh1clz50vtijpa.ix.internal.cloudapp.net>
2022-06-15 09:24:59 +0000

    Update visioncare plan web service

--- optic_store/api/vision_care_plan.py
+++ optic_store/api/vision_care_plan.py
@@ -34,3 +34,37 @@ def query_visioncare_plans(doctype, txt, searchfield, start, page_len, filters):
     )
 
 
+
+
+
+@frappe.whitelist()
+@frappe.validate_and_sanitize_search_inputs
+def query_visioncare_usage(doctype, txt, searchfield, start, page_len, filters):
+    query = """
+        SELECT b.name, b.plan_name, b.company_name, IFNULL(s.name, "Not Used"), IFNULL(s.usage_date, "Not Used")
+        FROM `tabVisioncare Plan Member` 
+        LEFT JOIN `tabVisioncare Plan Subscription` b
+        ON `tabVisioncare Plan Member`.parent = b.name
+        LEFT JOIN `tabVisioncare Plan Usage` s
+        ON s.subscription_plan = b.name
+        WHERE 
+            b.docstatus = 0
+            {fcond}
+        ORDER BY
+            IF(LOCATE(%(_txt)s, b.name), LOCATE(%(_txt)s, b.name), 99999),
+            b.valid_through DESC
+        LIMIT %(start)s, %(page_len)s
+    """.format(
+        key=searchfield, fcond=get_filters_cond('Visioncare Plan Member', filters, [])
+    )
+
+    print(query)
+    return frappe.db.sql(query,
+        values={
+            "txt": "%%%s%%" % txt,
+            "_txt": txt.replace("%", ""),
+            "start": start,
+            "page_len": page_len,
+        },
+    )
+
